fae06d8d7ca12af45e289c77801df8bc230c654d
Eric Fleming <eric-fleming18@hotmail.com>
2015-01-20 20:19:45 -0500

    Initial Commit of repo

---
+++ src/Identity2/Controllers/CreateCameraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Identity2.Controllers
+{
+    public class CreateCameraController : Controller
+    {
+        // GET: CreateCamera
+        public ActionResult CreateCamera()
+        {
+            return View();
+        }
+    }
+}
