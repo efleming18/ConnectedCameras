4d8742a9aec57d1a310bb5a64ec64d9f22b52446
Jeffrey Syme <jsyme16@gmail.com>
2015-01-27 18:12:29 -0500

    Added tap event for camera focus

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -2,3 +2,8 @@
     $(this).parent().parent().find('.cameraframe').css('border', 'solid 3px black');
     $(this).css('border', 'solid 3px red');
 })
+
+$('.cameraframe').on('tap', function () {
+    $(this).parent().parent().find('.cameraframe').css('border', 'solid 3px black');
+    $(this).css('border', 'solid 3px red');
+})
