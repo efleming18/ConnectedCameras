1b068d221d22e90f03d2ff5f65230629df905030
Jeffrey Syme <jsyme16@gmail.com>
2015-01-29 19:27:05 -0500

    Added a little camera control on the side.

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,6 +1,7 @@
 ﻿$('.cameraframe').click(function () {
     $(this).parent().parent().find('.cameraframe').css('border', 'solid 3px black');
     $(this).css('border', 'solid 3px red');
+    $('.camerainput').css('height', '265px');
 })
 
 $('.cameraframe').on('tap', function () {
