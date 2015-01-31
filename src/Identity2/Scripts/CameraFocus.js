9cf96ff8bf78cb3bb0ebe3774f901d6f72bd2f87
Jeffrey Syme <jsyme16@gmail.com>
2015-01-31 14:35:44 -0500

    Added jquery click events for feed sizes.

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,19 +1,35 @@
-﻿$('.cameraframe').click(function () {
-    $(this).parent().parent().find('.cameraframe').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px red');
-    if ($('.camerainput').height() < 200) {
-        $('.camerainput').animate({ height: '+=200px' });
-    }
-});
-$('.small-feed').click(function () {
+﻿$('.small-feed').click(function () {
     $(this).parent().parent().find('.small-feed').css('border', 'solid 2px black');
     $(this).css('border', 'solid 2px red');
     if ($('.camerainput').height() < 200) {
         $('.camerainput').animate({ height: '+=200px' });
     }
-})
+});
+$('.small-feed').on('tap', function () {
+    $(this).parent().parent().find('.small-feed').css('border', 'solid 3px black');
+    $(this).css('border', 'solid 3px red');
+});
 
-$('.cameraframe').on('tap', function () {
-    $(this).parent().parent().find('.cameraframe').css('border', 'solid 3px black');
+$('.medium-feed').click(function () {
+    $(this).parent().parent().find('.medium-feed').css('border', 'solid 2px black');
+    $(this).css('border', 'solid 2px red');
+    if ($('.camerainput').height() < 200) {
+        $('.camerainput').animate({ height: '+=200px' });
+    }
+});
+$('.medium-feed').on('tap', function () {
+    $(this).parent().parent().find('.medium-feed').css('border', 'solid 3px black');
+    $(this).css('border', 'solid 3px red');
+});
+
+$('.large-feed').click(function () {
+    $(this).parent().parent().find('.large-feed').css('border', 'solid 2px black');
+    $(this).css('border', 'solid 2px red');
+    if ($('.camerainput').height() < 200) {
+        $('.camerainput').animate({ height: '+=200px' });
+    }
+});
+$('.large-feed').on('tap', function () {
+    $(this).parent().parent().find('.large-feed').css('border', 'solid 3px black');
     $(this).css('border', 'solid 3px red');
 });
