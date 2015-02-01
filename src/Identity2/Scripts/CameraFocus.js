4d55cf4c1640e21f76560f33077a264e7c1c17ca
Jeffrey Syme <jsyme16@gmail.com>
2015-02-01 17:16:18 -0500

    Made a label for the current camera that is selected

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,35 +1,39 @@
 ﻿$('.small-feed').click(function () {
     $(this).parent().parent().find('.small-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px red');
-    if ($('.camerainput').height() < 200) {
-        $('.camerainput').animate({ height: '+=200px' });
-    }
+    $(this).css('border', 'solid 2px darkred');
+    ActivateCameraInput();
 });
 $('.small-feed').on('tap', function () {
     $(this).parent().parent().find('.small-feed').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px red');
+    $(this).css('border', 'solid 3px darkred');
+    ActivateCameraInput();
 });
 
 $('.medium-feed').click(function () {
     $(this).parent().parent().find('.medium-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px red');
-    if ($('.camerainput').height() < 200) {
-        $('.camerainput').animate({ height: '+=200px' });
-    }
+    $(this).css('border', 'solid 2px darkred');
+    ActivateCameraInput();
+    $('current-camera').html('')
 });
 $('.medium-feed').on('tap', function () {
     $(this).parent().parent().find('.medium-feed').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px red');
+    $(this).css('border', 'solid 3px darkred');
+    ActivateCameraInput();
 });
 
 $('.large-feed').click(function () {
     $(this).parent().parent().find('.large-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px red');
-    if ($('.camerainput').height() < 200) {
-        $('.camerainput').animate({ height: '+=200px' });
-    }
+    $(this).css('border', 'solid 2px darkred');
+    ActivateCameraInput();
 });
 $('.large-feed').on('tap', function () {
     $(this).parent().parent().find('.large-feed').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px red');
+    $(this).css('border', 'solid 3px darkred');
+    ActivateCameraInput();
 });
+
+function ActivateCameraInput() {
+    if ($('.camerainput').height() < 200) {
+        $('.camerainput').animate({ height: '+=250px' });
+    }
+}
