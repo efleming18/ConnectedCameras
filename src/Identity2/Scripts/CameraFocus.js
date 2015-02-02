61d6d2fce23c98e1c640ff386981fa458d20026c
Jeffrey Syme <jsyme16@gmail.com>
2015-02-01 22:17:46 -0500

    Simplified some of the jquery

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,39 +1,8 @@
-﻿$('.small-feed').click(function () {
-    $(this).parent().parent().find('.small-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px darkred');
-    ActivateCameraInput();
+﻿$('.live-feed').click(function () {
+    $(this).parent().find('.live-feed').css('border', 'solid 2px black');
+    $(this).css('border', 'solid 3px darkred');
 });
-$('.small-feed').on('tap', function () {
+$('.live-feed').on('tap', function () {
     $(this).parent().parent().find('.small-feed').css('border', 'solid 3px black');
     $(this).css('border', 'solid 3px darkred');
-    ActivateCameraInput();
 });
-
-$('.medium-feed').click(function () {
-    $(this).parent().parent().find('.medium-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px darkred');
-    ActivateCameraInput();
-    $('current-camera').html('')
-});
-$('.medium-feed').on('tap', function () {
-    $(this).parent().parent().find('.medium-feed').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px darkred');
-    ActivateCameraInput();
-});
-
-$('.large-feed').click(function () {
-    $(this).parent().parent().find('.large-feed').css('border', 'solid 2px black');
-    $(this).css('border', 'solid 2px darkred');
-    ActivateCameraInput();
-});
-$('.large-feed').on('tap', function () {
-    $(this).parent().parent().find('.large-feed').css('border', 'solid 3px black');
-    $(this).css('border', 'solid 3px darkred');
-    ActivateCameraInput();
-});
-
-function ActivateCameraInput() {
-    if ($('.camerainput').height() < 200) {
-        $('.camerainput').animate({ height: '+=250px' });
-    }
-}
