a340d2a1dcb21d3da80d25f9145e413e7b7449b9
Jeffrey Syme <jsyme16@gmail.com>
2015-02-03 14:33:50 -0500

    Displays which camera has the focus in the bottom

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,6 +1,8 @@
 ﻿$('.live-feed').click(function () {
     $(this).parent().find('.live-feed').css('border', 'solid 2px black');
     $(this).css('border', 'solid 3px darkred');
+    var cameraName = $(this).attr('id');
+    $('#currentCamera').html(cameraName);
 });
 $('.live-feed').on('tap', function () {
     $(this).parent().parent().find('.small-feed').css('border', 'solid 3px black');
