f28dd2eb913fd3fc91186fc7cc7bed8710666fc7
Jeffrey Syme <jsyme16@gmail.com>
2015-02-10 19:07:07 -0500

    improved styling of the camera picker

---
+++ src/Identity2/Scripts/cameraPicking.js
@@ -0,0 +1,9 @@
+﻿$('.row90percent').mouseover(function () {
+    $(this).css('background-color', 'aqua');
+});
+$('.row90percent').mouseleave(function () {
+    $(this).css('background-color', 'transparent');
+})
+$('.row90percent').click(function () {
+
+});
