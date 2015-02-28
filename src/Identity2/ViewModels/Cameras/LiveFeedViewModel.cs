a5289ab148aa4476b000af61f147897bc7d41bfa
Jeffrey Syme <jsyme16@gmail.com>
2015-02-28 12:12:55 -0500

    New feed styling supports up to 2 cameras

--- src/Identity2/ViewModels/Cameras/LiveFeedViewModel.cs
+++ src/Identity2/ViewModels/Cameras/LiveFeedViewModel.cs
@@ -18,5 +18,45 @@ namespace ConnectedCamerasWeb.ViewModels.Cameras
                 return "medium-feed";
             return "small-feed";
         }
+        //We could turn this into an Html helper maybe...
+        public IHtmlString GenerateFeedHtml()
+        {
+            string htmlString;
+            switch (Cameras.Count)
+            {
+                case 1:
+                    htmlString = string.Format("<div id='{0}' class='row100percent'>" + 
+                                                   "<img class='fill center' src='../../Content/Images/cat.gif' />" + 
+                                               "</div>",
+                                                Cameras[0].CameraName);
+                    break;
+                case 2:
+                    htmlString = string.Format("<div id='{0}' class='row50percent'>" + 
+                                                   "<img class='fill center' src='../../Content/Images/cat.gif' />" + 
+                                               "</div>" + 
+                                               "<div id='{1}' class='row50percent'>" + 
+                                                    "<img class='fill center' src='../../Content/Images/cat.gif' />" + 
+                                                "</div>",
+                                                Cameras[0].CameraName,
+                                                Cameras[1].CameraName);
+                    break;
+                case 3:
+                    htmlString = "";
+                    break;
+                case 4:
+                    htmlString = "";
+                    break;
+                case 5:
+                    htmlString = "";
+                    break;
+                case 6:
+                    htmlString = "";
+                    break;
+                default:
+                    htmlString = "We do not support this many cameras.";
+                    break;
+            }
+            return new HtmlString(htmlString);
+        }
     }
 }
