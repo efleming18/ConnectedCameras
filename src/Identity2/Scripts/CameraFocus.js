c2f49240faa1b8520beeb4be2a0e97deda111637
Jeffrey Syme <jsyme16@gmail.com>
2015-03-02 21:05:00 -0500

    Gave the arrows a better style

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -11,23 +11,12 @@ feed.click(function () {
     $(this).css('border', 'solid 2px red');
 });
 
+$('.camera-input').mouseover(function () {
 
+});
 $('.camera-input').mousedown(function () {
-    $(this).css('background-color', '#4bacbd');
+
 });
 $('.camera-input').mouseup(function () {
-    $(this).css('background-color', 'lightgray');
-});
 
-$('.arrow-up').mousedown(function () {
-    $(this).parent().css('background-color', '#4bacbd')
-});
-$('.arrow-down').mousedown(function () {
-    $(this).parent().css('background-color', '#4bacbd')
-});
-$('.arrow-left').mousedown(function () {
-    $(this).parent().css('background-color', '#4bacbd')
-});
-$('.arrow-right').mousedown(function () {
-    $(this).parent().css('background-color', '#4bacbd')
 });
