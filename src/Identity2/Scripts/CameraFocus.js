4ab2b5bb4af08dbaf5950e20f0e6e9e95ccf3e61
Eric Fleming <eric-fleming18@hotmail.com>
2015-03-03 13:28:28 -0500

    Jeff - Camera input is docked at the bottom of the control-panel

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,15 +1,25 @@
 ﻿var feed = $('.fill');
 
 $(document).ready(function () {
-    var fixHeight = $('.camera-controller').css('height');
-    var fixWidth = $('.camera-controller').css('width');
-    $('.camera-controller').height(fixHeight);
-    $('.camera-controller').width(fixWidth);
+    setCameraControllerHeight();
+    setAttributesPanelHeight();
 });
 feed.click(function () {
     $(document).find('.fill').css('border', '');
     $(this).css('border', 'solid 2px red');
 });
+function setCameraControllerHeight() {
+    var fixHeight = $('.camera-controller').css('height');
+    var fixWidth = $('.camera-controller').css('width');
+    $('.camera-controller').height(fixHeight);
+    $('.camera-controller').width(fixWidth);
+}
+function setAttributesPanelHeight() {
+    var totalHeight = $('.camera-controller').height();
+    var controlsHeight = $('#controls').height();
+    var attributeHeight = totalHeight - controlsHeight;
+    $('#attributes').height(attributeHeight);
+}
 
 $('.camera-input').mouseover(function () {
 
