f4341e69291f18ca6bf8bdd3772232739a77f758
Jeffrey Syme <jsyme16@gmail.com>
2015-03-03 18:07:10 -0500

    Added mouse-over and mouse-down events for the input arrows

--- src/Identity2/Scripts/CameraFocus.js
+++ src/Identity2/Scripts/CameraFocus.js
@@ -1,13 +1,32 @@
 ﻿var feed = $('.fill');
+var cameraInput = $('.camera-input');
+
+var arrowMouseLeave = "../../Content/Images/Arrow.png";
+var arrowMouseOver = "../../Content/Images/Arrow-mouse-over.png";
+var arrowMouseDown = "../../Content/Images/Arrow-mouse-down.png";
 
 $(document).ready(function () {
     setCameraControllerHeight();
     setAttributesPanelHeight();
+
+    feed.click(function () {
+        $(document).find('.fill').css('border', '');
+        $(this).css('border', 'solid 2px red');
+    });
+    cameraInput.mouseover(function () {
+        $(this).attr("src", arrowMouseOver);
+    });
+    cameraInput.mouseleave(function () {
+        $(this).attr("src", arrowMouseLeave);
+    });
+    cameraInput.mousedown(function () {
+        $(this).attr("src", arrowMouseDown);
+    });
+    cameraInput.mouseup(function () {
+        $(this).attr("src", arrowMouseOver);
+    });
 });
-feed.click(function () {
-    $(document).find('.fill').css('border', '');
-    $(this).css('border', 'solid 2px red');
-});
+
 function setCameraControllerHeight() {
     var fixHeight = $('.camera-controller').css('height');
     var fixWidth = $('.camera-controller').css('width');
@@ -20,10 +39,6 @@ function setAttributesPanelHeight() {
     var attributeHeight = totalHeight - controlsHeight;
     $('#attributes').height(attributeHeight);
 }
-
-$('.camera-input').mouseover(function () {
-
-});
 $('.camera-input').mousedown(function () {
 
 });
