90416a0dd8253f3fceb4e0d479da17f0a5c5ae04
Eric Fleming <eric-fleming18@hotmail.com>
2015-04-16 15:19:43 -0400

    Jeff - Re-did the vlc plugin

--- src/Identity2/Helpers/HtmlExtensions.cs
+++ src/Identity2/Helpers/HtmlExtensions.cs
@@ -15,48 +15,48 @@ namespace ConnectedCamerasWeb.Helpers
             switch (cameras.Count)
             {
                 case 1:
-                    htmlString = string.Format("<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>",
+                    htmlString = string.Format("<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>",
                                    cameras[0].CameraUrl);
                     break;
                 case 2:
                     htmlString = string.Format("<div class='row50percent'>" +
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" +
                                  "</div>" +
                                  "<div class='row50percent'>" +
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" +
                                  "</div>", cameras[0].CameraUrl,
                                            cameras[1].CameraUrl);
                     break;
                 case 3:
                     htmlString = string.Format("<div class='row50percent'>" +
                                     "<div class='cell50percent'>" +
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" +
                                     "</div>" +
                                     "<div class='cell50percent'>" +
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{1}' style='z-index:5;'></embed>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{1}' style='z-index:5;'></embed>" +
                                     "</div>" +
                                  "</div>" +
                                  "<div class='row50percent'>" +
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" +
                                  "</div>", cameras[0].CameraUrl,
                                            cameras[1].CameraUrl,
                                            cameras[2].CameraUrl);
                     break;
                 case 4:
                     htmlString = string.Format("<div class='row50percent'>" + 
-                                    "<div class='cell50percent'>" + 
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" + 
+                                    "<div class='cell50percent'>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{0}' style='z-index:5;'></embed>" + 
                                     "</div>" + 
-                                    "<div class='cell50percent'>" + 
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{1}' style='z-index:5;'></embed>" + 
+                                    "<div class='cell50percent'>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{1}' style='z-index:5;'></embed>" + 
                                     "</div>" + 
                                  "</div>" + 
                                  "<div class='row50percent'>" + 
-                                    "<div class='cell50percent'>" + 
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" + 
+                                    "<div class='cell50percent'>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{2}' style='z-index:5;'></embed>" + 
                                     "</div>" + 
-                                    "<div class='cell50percent'>" + 
-                                        "<embed type='application/x-vlc-plugin' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{3}' style='z-index:5;'></embed>" + 
+                                    "<div class='cell50percent'>" +
+                                        "<embed type='application/x-vlc-plugin' text='This stream cannot be accessed at this time...' controls='false' name='VLC' autoplay='yes' loop='no' volume='100' width='100%' height='100%' target='{3}' style='z-index:5;'></embed>" + 
                                     "</div>" + 
                                  "</div>", cameras[0].CameraUrl, 
                                            cameras[1].CameraUrl,
